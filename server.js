182d52ea28776beec8e8a76c7e29186a4e41a771
chaurasiyanandini379-gif <nandini.chaurasiya@vpt.edu.in>
2026-06-19 00:03:40 +0530

    Implemented user registration API

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 
 connectDB();
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("API Working");
 });
