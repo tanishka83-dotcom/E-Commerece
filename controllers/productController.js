7f61b912aff5acd678470a62613d072b981e7c8f
chaurasiyanandini379-gif <nandini.chaurasiya@vpt.edu.in>
2026-06-19 14:48:21 +0530

    Implemented update product API

--- controllers/productController.js
+++ controllers/productController.js
@@ -26,7 +26,31 @@ const getProducts = async (req, res) => {
   }
 };
 
+// Update Product
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createProduct,
   getProducts,
+  updateProduct,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,9 +6,11 @@ const protect = require("../middleware/authMiddleware");
 const {
   createProduct,
   getProducts,
+  updateProduct,
 } = require("../controllers/productController");
 
 router.post("/", protect, createProduct);
 router.get("/", protect, getProducts);
+router.put("/:id", protect, updateProduct);
 
 module.exports = router;
