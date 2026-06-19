aacb2537e5e081e933a72fb9a3bd43a98d4f90dd
chaurasiyanandini379-gif <nandini.chaurasiya@vpt.edu.in>
2026-06-19 13:58:58 +0530

    Implemented JWT authentication middleware

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
+
 dotenv.config();
 
 connectDB();
@@ -9,21 +11,17 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.use((req, res) => {
-  res.status(404).json({
-    message: "Route not found",
-  });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).json({
-    message: err.message || "Internal server error",
+app.get("/protected", protect, (req, res) => {
+  res.json({
+    message: "Protected Route Accessed",
+    user: req.user
   });
 });
 
